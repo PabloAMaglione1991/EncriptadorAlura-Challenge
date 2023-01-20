d38b3e35aefac522752eadd77c545ee4b21c5bd4
PabloAMaglione1991 <pablo.maglione1991@gmail.com>
2023-01-20 12:07:23 -0300

    Proyecto finalizado
    Final del proyecto Encriptador Alura

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center">Encriptador de texto</h1>
+<h3 align="center">Programa web desarrollado para Alura Latam, como parte del programa ONE</h3>
+
+## Introducción
+Vamos a trabajar en una aplicación que encripta textos, así podrás intercambiar mensajes secretos con otras personas que sepan el secreto de la encriptación utilizada.
+
+- **Las "llaves" de encriptación que utilizaremos son las siguientes**
+
+```bash
+La letra "a" es convertida para "ai"
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+```
+
+- **Requisitos**
+> Debe funcionar solo con letras minúsculas
+> No deben ser utilizados letras con acentos ni caracteres especiales
+> Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
+```bash
+Por ejemplo:
+"gato" => "gaitober"
+"gaitober" => "gato"
+```
+
+- **La página debe tener campos para**
+> Inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
+> El resultado debe ser mostrado en la pantalla.
---
+++ script/script.js
@@ -0,0 +1,116 @@
+var showimg;
+const excluidos = /[A-Z\ á é í ó ú]/;
+
+var textIn = document.querySelector("#entrada-de-texto");
+var textOut = document.querySelector("#resultado-encriptado");
+
+var botonEncriptar = document.querySelector("#btn-encriptar");
+var botonDesencriptar = document.querySelector("#btn-desencriptar");
+var botonCopiar = document.querySelector("#btn-copiar");
+
+var campo_textresult = document.querySelector(".resultado-salida").style.display = "none";
+var campo_btncopy = document.querySelector(".btn-copiar").style.display = "none";
+
+function encriptar (){
+	let aux;
+	let word = "";
+	let texto = textIn.value;
+
+	if(excluidos.test(texto)){
+		texto = "";
+		alert("No esta permitido usar mayusculas o acentos, ingrese nuevamente su texto");
+	}
+	if(texto == ""){
+		notshowres();
+	}else{
+		showres();
+	}
+
+	for(var i=0; i<texto.length; i++){
+		switch (texto.charAt(i)) {
+            case 'a':
+                aux = "ai";
+                break;
+            case 'e':
+                aux = "enter";
+                break;
+            case 'i':
+                aux = "imes";
+                break;
+            case 'o':
+                aux = "ober";
+                break;
+            case 'u':
+                aux = "ufat";
+                break;
+            default:
+                aux = texto.charAt(i);
+                break;
+        }
+		word = word + aux;
+	}
+	textOut.value = word;
+}
+
+function desencriptar (){
+	let aux;
+	let word = "";
+	let texto = textIn.value;
+
+	if(excluidos.test(texto)){
+		texto = "";
+		alert("No esta permitido usar mayusculas o acentos, ingrese nuevamente su texto");
+	}
+	if(texto == ""){
+		notshowres();
+	}else{
+		showres();
+	}
+
+	for (let i = 0; i < texto.length; i++) {
+        switch (texto.charAt(i)) {
+            case 'a':
+                aux = i + 1;
+                break;
+            case 'e':
+                aux = i + 4;
+                break;
+            case 'i':
+                aux = i + 3;
+                break;
+            case 'o':
+                aux = i + 3;
+                    break;
+            case 'u':
+                aux = i + 3;
+                break;
+            default:
+                aux = i;
+                break;
+        }
+        word = word + texto.charAt(i);
+        i = aux;
+    }
+    textOut.value = word;
+}
+
+function copiar () {
+    let textoEncriptado = textOut.value;
+    navigator.clipboard.writeText(textoEncriptado);
+    alert("Mensaje copiado");
+}
+
+function showres() {
+    showimg = document.querySelector("#primera-fase").style.display = "none";
+    campo_textresult = document.querySelector("#resultado-encriptado").style.display = "block"
+    campo_btncopy = document.querySelector(".btn-copiar").style.display = "inline-flex";
+}
+
+function notshowres() {
+    showimg = document.querySelector("#primera-fase").style.display = "block";
+    campo_textresult = document.querySelector("#segunda-fase").style.display = "none"
+}
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+botonCopiar.onclick = copiar;
